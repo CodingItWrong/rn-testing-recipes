e5f7a57ff28db00b4d26cc47375f11341fa7385d
Josh Justice <me@codingitwrong.com>
2022-08-15 07:23:44 -0500

    Test confirmation

--- src/NativeComponentsScreen/NativeComponentsScreen.js
+++ src/NativeComponentsScreen/NativeComponentsScreen.js
@@ -1,13 +1,29 @@
+import {useState} from 'react';
 import {Alert, Pressable, Text} from 'react-native';
 
 export default function NativeComponentsScreen() {
+  const [confirmationResult, setConfirmationResult] = useState(null);
+
   function showAlert() {
     Alert.alert('Alert Title', 'This is an alert', [{text: 'OK'}]);
   }
 
+  function showConfirmation() {
+    Alert.alert('Confirmation', 'Do you agree?', [
+      {text: 'No', onPress: () => setConfirmationResult('tapped No')},
+      {text: 'Yes', onPress: () => setConfirmationResult('tapped Yes')},
+    ]);
+  }
+
   return (
-    <Pressable onPress={showAlert}>
-      <Text>Show Alert Message</Text>
-    </Pressable>
+    <>
+      <Pressable onPress={showAlert}>
+        <Text>Show Alert Message</Text>
+      </Pressable>
+      <Pressable onPress={showConfirmation}>
+        <Text>Show Confirmation</Text>
+      </Pressable>
+      {confirmationResult && <Text>{confirmationResult}</Text>}
+    </>
   );
 }
--- src/NativeComponentsScreen/NativeComponentsScreen.spec.js
+++ src/NativeComponentsScreen/NativeComponentsScreen.spec.js
@@ -1,8 +1,12 @@
-import {fireEvent, render, screen} from '@testing-library/react-native';
+import {act, fireEvent, render, screen} from '@testing-library/react-native';
 import {Alert} from 'react-native';
 import NativeComponentsScreen from './NativeComponentsScreen';
 
 describe('NativeComponentsScreen', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
   it('can test that an alert was shown', () => {
     jest.spyOn(Alert, 'alert');
     // TODO: clear mocks afterward?
@@ -16,5 +20,26 @@ describe('NativeComponentsScreen', () => {
       'This is an alert',
       [{text: 'OK'}],
     );
+  });
+
+  it('can trigger button actions', () => {
+    jest.spyOn(Alert, 'alert');
+
+    render(<NativeComponentsScreen />);
+
+    fireEvent.press(screen.getByText('Show Confirmation'));
+    const alertButtons = Alert.alert.mock.calls[0][2];
+
+    act(() => {
+      alertButtons.find(button => button.text === 'No').onPress();
+    });
+
+    expect(screen.queryByText('tapped No')).toBeTruthy();
+
+    act(() => {
+      alertButtons.find(button => button.text === 'Yes').onPress();
+    });
+
+    expect(screen.queryByText('tapped Yes')).toBeTruthy();
   });
 });
